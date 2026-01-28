dc65361914fe9f096e657add85c75f297cb7698a
phamd2 <phamd@stpaulsschool.org.uk>
2026-01-28 10:40:06 +0000

    code

--- OOPDraw/OOPDraw.cs
+++ OOPDraw/OOPDraw.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace OOPDraw
 {
     public partial class OOPDraw : Form
@@ -15,19 +9,40 @@ namespace OOPDraw
         public OOPDraw()
         {
             InitializeComponent();
-            DoubleBuffered = true;
+            DoubleBuffered = true; //Stops image flickering
         }
-
         Pen currentPen = new Pen(Color.Black);
+        bool dragging = false;
+        Point startOfDrag = Point.Empty;
+        Point lastMousePosition = Point.Empty;
+        List<Line> lines = new List<Line>();
         private void Canvas_Paint(object sender, PaintEventArgs e)
         {
             Graphics gr = e.Graphics;
-            Point a = new Point(20, 30);
-            Point b = new Point(400, 500);
-            Point c = new Point(400, 100);
-            gr.DrawLine(currentPen, a, b);
-            gr.DrawLine(currentPen, b, c);
-            gr.DrawLine(currentPen, c, a);
+            foreach (Line line in lines)
+            {
+                line.Draw(gr);
+            }
+        }
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            startOfDrag = lastMousePosition = e.Location;
+            lines.Add(new Line(currentPen, e.X, e.Y));
+        }
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Line currentLine = lines.Last();
+                currentLine.GrowTo(e.X, e.Y);
+                lastMousePosition = e.Location;
+                Refresh();
+            }
+        }
+        private void Canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
         }
     }
 }
